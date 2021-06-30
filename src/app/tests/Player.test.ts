de5c2cab65e1ec785c7645399e76717acc95db69
romservik29 <andrei.romservik29@gmail.com>
2021-06-30 19:19:51 +0300

    player tests

---
+++ src/app/tests/Player.test.ts
@@ -0,0 +1,13 @@
+import { Player } from './../Player';
+describe('Player methods', () => {
+    it('plus chips', ()=>{
+        const player = new Player("asd",5000)
+        player.addChips(5000)
+        expect(player.chips).toBe(10000)
+    })
+    it('minus chips', ()=>{
+        const player = new Player("asd",5000)
+        player.minusChips(5000)
+        expect(player.chips).toBe(0)
+    })
+})
